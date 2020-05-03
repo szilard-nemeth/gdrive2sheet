50218711d6665c90f5eabef90c169f6bdd796d94
Szilard Nemeth <szilard.nemeth88@gmail.com>
2020-05-03 22:10:43 +0200

    list_shared_files.py: Extracted variables, code cleanup

--- list_shared_files.py
+++ list_shared_files.py
@@ -21,12 +21,16 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
+PORT = 49555
+ORDER_BY = "sharedWithMeTime desc"
+PAGESIZE = 100
+QUERY = "sharedWithMe"
+FIELDS = ["id", "name", "mimeType", "webViewLink", "createdTime", "modifiedTime", "sharedWithMeTime", "sharingUser", "owners"]
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
+    """
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -42,7 +46,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=49555)
+            creds = flow.run_local_server(port=PORT)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
@@ -51,8 +55,10 @@ def main():
 
     files = service.files()
     # File fields are documented here: https://developers.google.com/drive/api/v3/reference/files#resource
-    request = files.list(q="sharedWithMe", pageSize=100, fields="nextPageToken, files(id, name, mimeType, webViewLink, createdTime, modifiedTime, sharedWithMeTime, sharingUser, owners)",
-                         orderBy="sharedWithMeTime desc")
+    fields_str = ", ".join(FIELDS)
+    file_fields = "files(" + fields_str + ")"
+    fields = "nextPageToken, " + file_fields
+    request = files.list(q=QUERY, pageSize=PAGESIZE, fields=fields, orderBy=ORDER_BY)
     while request is not None:
         files_doc = request.execute()
         list_files(files_doc)

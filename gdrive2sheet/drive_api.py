a0b18697194c50438ad7433c3d2b04061263a392
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-03-27 16:17:44 +0100

    Move DriveAuthorizer to python-commons

--- gdrive2sheet/drive_api.py
+++ gdrive2sheet/drive_api.py
@@ -1,17 +1,14 @@
 import logging
-import pickle
-import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from pythoncommons.google_api import GoogleApiAuthorizer
 from pythoncommons.string_utils import StringUtils, auto_str
 
 LOG = logging.getLogger(__name__)
 
 
 class DriveApiMimeTypes:
-    #https://stackoverflow.com/questions/4212861/what-is-a-correct-mime-type-for-docx-pptx-etc
-    #https://stackoverflow.com/questions/11894772/google-drive-mime-types-listing
+    # https://stackoverflow.com/questions/4212861/what-is-a-correct-mime-type-for-docx-pptx-etc
+    # https://stackoverflow.com/questions/11894772/google-drive-mime-types-listing
     MAPPINGS = {
         "application/vnd.openxmlformats-officedocument.presentationml.presentation": "MS Presentation (pptx)",
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "MS Word document (docx)",
@@ -34,6 +31,7 @@ class DriveApiMimeTypes:
         "application/vnd.google-apps.presentation": "Google presentation",
         "application/vnd.google-apps.map": "Google map",
     }
+
 
 class DriveApiFileFields:
     F_OWNER = "owners"
@@ -110,7 +108,7 @@ class DriveApiWrapper:
     Q_SHARED_WITH_ME = "sharedWithMe"
 
     def __init__(self):
-        self.authorizer = DriveAuthorizer()
+        self.authorizer = GoogleApiAuthorizer()
         self.creds = self.authorizer.authorize()
 
     def print_shared_files(self, page_size=100, fields=None, order_by=ORDER_BY_DEFAULT):
@@ -173,51 +171,3 @@ class DriveApiWrapper:
     def print_files(items):
         for item in items:
             LOG.info(u'{0} ({1})'.format(item[DriveApiFileFields.NAME], item[DriveApiFileFields.ID]))
-
-
-class DriveAuthorizer:
-    CREDENTIALS_FILENAME = 'credentials.json'
-    TOKEN_FILENAME = 'token.pickle'
-    # If modifying these scopes, delete the file token.pickle.
-    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-    SERVER_PORT = 49555
-
-    def __init__(self):
-        pass
-
-    def authorize(self):
-        creds = self._load_token()
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            creds = self._handle_login(creds)
-        return creds
-
-    @classmethod
-    def _load_token(cls):
-        """
-        The file token.pickle stores the user's access and refresh tokens, and is
-        created automatically when the authorization flow completes for the first
-        time.
-        """
-        creds = None
-        if os.path.exists(cls.TOKEN_FILENAME):
-            with open(cls.TOKEN_FILENAME, 'rb') as token:
-                creds = pickle.load(token)
-        return creds
-
-    @classmethod
-    def _handle_login(cls, creds):
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                cls.CREDENTIALS_FILENAME, cls.SCOPES)
-            creds = flow.run_local_server(port=cls.SERVER_PORT)
-        # Save the credentials for the next run
-        cls._write_token(creds)
-        return creds
-
-    @classmethod
-    def _write_token(cls, creds):
-        with open(cls.TOKEN_FILENAME, 'wb') as token:
-            pickle.dump(creds, token)
